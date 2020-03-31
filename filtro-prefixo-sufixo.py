34e185d3ee3b655b362f58851d23265815330c7c
= <=>
2020-03-31 18:47:43 -0300

    Criação do método write_tfidf_pairs_binary e read_tfidf_pairs_binary.

--- filtro-prefixo-sufixo.py
+++ filtro-prefixo-sufixo.py
@@ -69,6 +69,7 @@ class text(object):
 
     #Grava arquivo binário com os documento e features, formato:
     #doc_id feature1_id feature1_value feature2_id feature2_value feature3_id feature3_value ...
+    #Grava o id do documento, a quantidade de features do documento e grava todos os id's de features juntos e depois todos os valores das features.
     def write_tfidf_binary(self, featU, out_path = 'bin_file_tfidf'):
 
         fd_out = open(out_path, 'wb')
@@ -94,6 +95,48 @@ class text(object):
             entry = struct.pack(fmt, doc_id, count, *feature_id, *feature_value)
             fd_out.write(entry)
             fd_out.flush()
+            doc_id += 1
+
+        fd_out.close()
+
+    #Grava arquivo binário com os documento e features, formato:
+    #doc_id feature_amount feature1_id feature1_value feature2_id feature2_value feature3_id feature3_value ...
+    # Grava o id do documento, a quantidade de features do documento e grava os pares id e valor das features juntos.
+    def write_tfidf_pairs_binary(self, featU, out_path = 'bin_file_tfidf'):
+
+        fd_out = open(out_path, 'wb')
+
+        # [4-byte ID_DOC][4-byte FEATURE_AMOUNT][4-byte FEATURE_ID][4-byte FEATURE_VALUE]...
+        # format: <LIIf
+
+        doc_id = 0
+        feature_id = []
+        feature_value = []
+        fmt = ''
+        for d in featU:
+            count = 0;
+            feature_id = []
+            feature_value = []
+            for feat_id in range(len(d)):
+                if (d[feat_id] != 0):
+                    count += 1
+                    feature_id.append(feat_id)
+                    feature_value.append(d[feat_id])
+
+            #Grava o id do documento e a quantidade de features do documento.
+            fmt = '<LI'
+            entry = struct.pack(fmt, doc_id, count)
+            fd_out.write(entry)
+            fd_out.flush()
+
+            #Percorre os vetores com os id's e valores das features
+            for i in range(feature_id.__len__()):
+                #Grava cada par de id e valor das features.
+                fmt = '<If'
+                entry = struct.pack(fmt, feature_id[i], feature_value[i])
+                fd_out.write(entry)
+                fd_out.flush()
+
             doc_id += 1
 
         fd_out.close()
@@ -163,29 +206,53 @@ class text(object):
                 break
         fid.close()
 
-        #file = open(fname, 'rb')
+    # Lê arquivo binário com os documento e features, formato:
+    # record size (quantidade de feature_id e feature value do documento) doc_id feature1_id feature1_value feature2_id feature2_value feature3_id feature3_value ...
+    def read_tfidf_pairs_binary(self, fname='bin_file_tfidf'):
+        # file = open(fname, 'rb')
 
-        # [4-byte RECORD SIZE][4-byte ID_DOC][4-byte FEATURE_ID][4-byte FEATURE_VALUE]
-        # format: <IL999999999999If
+        # [4-byte ID_DOC][4-byte RECORD SIZE][4-byte FEATURE_ID][4-byte FEATURE_VALUE]
+        # format: <LIIf
+        docs = []
+        pairs = []
+        feature = []
+        #Abre arquivo binário para leitura
+        with open(fname, "rb") as fid:
+            while True:
+                for i in range(2):
+                    pairs = []
 
-        # with open(fname, "rb") as fid:
-        #     while True:
-        #         for i in range(2):
-        #             size_record = fid.read(4)
-        #             if not size_record: break
-        #
-        #             size = struct.unpack('<I', size_record)
-        #             fmt = '<L'+str(int((size[0]-1)/2))+'I'+str(int((size[0]-1)/2))+'f'
-        #
-        #             record = fid.read(size[0])
-        #
-        #             if not record: break
-        #
-        #             tupla, = struct.unpack(fmt, record)
-        #         else:
-        #             continue
-        #         break
-        # fid.close()
+                    #Recupera os primeiros 4 bytes do arquivo com o id do documento.
+                    b_id_doc = fid.read(4)
+                    if not b_id_doc: break
+                    #armazena o id do documento codificado em numérico (long) na variável id_doc
+                    id_doc = struct.unpack('<L', b_id_doc)
+                    #adiciona o id do documento em uma lista.
+                    docs.append(id_doc[0])
+
+                    #recupera os próximos 4 bytes do arquico com a quantidade de features do documento.
+                    size_record = fid.read(4)
+                    if not size_record: break
+
+                    #armazena a quantidade codificada em numérico (integer) na variável size.
+                    size = struct.unpack('<I', size_record)
+
+                    sz = size[0]
+                    #Laço para percorrer a quantidade de features (feature_id) que o documento tem.
+                    for i in range((sz)):
+                        #recupera o id da feature armazenado em 4 bytes.
+                        b_feature = fid.read(8)
+                        if not b_feature: break
+                        #armazena o id da feature codificada em numérico (integer).
+                        pair = struct.unpack('<If', b_feature)
+                        pairs.append(pair)
+
+                    # adiciona os vetores com os id's e valores das feautures.
+                    feature.append(pairs)
+                else:
+                    continue
+                break
+        fid.close()
 
 
 class cosine(object):
@@ -485,6 +552,7 @@ featU = cosine.toUnitMatrix(feat)
 
 
 #read.write_tfidf(featU, "dataset-20.txt")
+#read.write_tfidf__binary(featU, 'bin-dataset-20')
 read.write_tfidf_binary(featU, 'bin-dataset-20')
 read.read_tfidf_binary('bin-dataset-20')
 
